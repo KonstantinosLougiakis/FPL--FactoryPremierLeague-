8280545b34eec6226aefb560994008c5e50c9ea5
KonstantinosLougiakis <lougiokostas@gmail.com>
2024-06-14 17:00:33 +0300

    new component. person-table

--- FPLApp-Front/README.md
+++ FPLApp-Front/README.md
@@ -1,3 +1,5 @@
+# Step 3: Creating a new component named person-table
+
 # Step 2: Creating the first person in the application, presenting the personal information given
 
 # Step 1: Make the initial set up for the project
--- FPLApp-Front/src/app/app.component.ts
+++ FPLApp-Front/src/app/app.component.ts
@@ -1,20 +1,14 @@
 import { Component } from '@angular/core';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'FPL-App';
   name = 'Konstantinos';
-
-  person = {
-    givenName: 'Konstantinos',
-    surName: 'Lougiakis',
-    age: 27,
-    email: 'lougiokostas@gmail.com'
-  }
 }
