bd407b1e9b4cbd87eba1629b5e6c2892c6098df8
KonstantinosLougiakis <lougiokostas@gmail.com>
2024-06-15 11:51:42 +0300

    person-component

--- FPLApp-Front/README.md
+++ FPLApp-Front/README.md
@@ -1,3 +1,5 @@
+# Step 4: Creating a component in which the personal information of each user of the app is written in
+
 # Step 3: Creating a new component named person-table
 
 # Step 2: Creating the first person in the application, presenting the personal information given
--- FPLApp-Front/src/app/app.component.ts
+++ FPLApp-Front/src/app/app.component.ts
@@ -11,4 +11,18 @@ import { PersonTableComponent } from './components/person-table/person-table.com
 export class AppComponent {
   title = 'FPL-App';
   name = 'Konstantinos';
+
+  person0 = {
+    givenName: 'Konstantinos',
+    surName: 'Lougiakis',
+    age: 27,
+    email: 'lougiokostas@gmail.com'
+  };
+
+  person1 = {
+    givenName: 'Nikos',
+    surName: 'Papadopoulos',
+    age: 34,
+    email: 'papanikos@gmail.com'
+  };
 }
